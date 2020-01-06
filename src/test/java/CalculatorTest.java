dcd9447fe17e9f55d6f3500e817cf90e095e952f
Catrin <catrinhughes92@gmail.com>
2020-01-06 14:54:57 +0000

    multiply test added

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -19,4 +19,9 @@ public class CalculatorTest {
     public void canSubtract() {
         assertEquals(10.00, calculator.subtract(15.00, 5.00), 0.01);
     }
+
+    @Test
+    public void canMultiply() {
+        assertEquals(100.00, calculator.multiply(10.00, 10.00), 0.01);
+    }
 }
