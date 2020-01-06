b2cec8822b7f8d04ee7796e67f4a2cb3a005a10a
Catrin <catrinhughes92@gmail.com>
2020-01-06 14:55:50 +0000

    multiply methods added, third test passes

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,4 +7,8 @@ public class Calculator {
     public double subtract(double x, double y) {
         return x - y;
     }
+
+    public double multiply(double x, double y) {
+        return x * y;
+    }
 }
