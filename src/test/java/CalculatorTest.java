6a74530485d0ef9c7449e7792974bb423242356b
Catrin <catrinhughes92@gmail.com>
2020-01-06 14:48:58 +0000

    add test created

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator;
+    @Before
+    public void before() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd() {
+        assertEquals(10.00, calculator.add(5.00, 5.00));
+    }
+}
