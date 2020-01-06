9c621e220d4899b2520030e8f8abc5542791c32c
Catrin <catrinhughes92@gmail.com>
2020-01-06 14:53:39 +0000

    subtract method added, second test passed

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,4 +3,8 @@ public class Calculator {
     public double add(double x, double y) {
         return x + y;
     }
+
+    public double subtract(double x, double y) {
+        return x - y;
+    }
 }
